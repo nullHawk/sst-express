7fe486b3d1e878a3c22c4db8c5ef2cde04031c51
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2024-07-03 10:10:36 +0530

    added post and put

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
+app.use(express.json());
+
 const port = 8000;
 
 let courses = [
@@ -10,6 +11,21 @@ let courses = [
 ];
 
 app.get("/courses", (req,res) =>{
+    res.json(courses);
+});
+app.post("/courses",(req,res)=>{
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.json(courses);
+});
+
+app.put("/courses/:id", (req,res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with the given ID was not found');
+    course.name = req.body.name;
     res.json(courses);
 });
 
